0163eb660b31d1f1f73720cc52611e8ce34416b2
Wender Lima <vasconceloswender98@gmail.com>
2022-11-29 21:49:45 -0300

    v1 - Css concluído

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import FormLogo from './assets/form-logo.png'
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello React
-    </div>
+    <form>
+      <img src={FormLogo} alt="imagem-logo" />
+
+      <label>
+        Nome
+        <input />
+      </label>
+      
+      <label>
+        Email
+        <input />
+      </label>
+
+      <label>
+        Senha
+        <input />
+      </label>
+
+      <label>
+        Confirmar Senha
+        <input />
+      </label>
+
+      <button type="Submit" >Cadastrar-se</button>
+    </form>
   );
 }
 
